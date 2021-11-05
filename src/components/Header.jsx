44e24f672daeae63a8e451b9540ac2fab99ef126
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-05 13:06:57 +0200

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src='https://mundstuck.com.ua/wp-content/uploads/2020/09/m_white.png' />
+        </header>
+    );
+}
+
+export default Header;
