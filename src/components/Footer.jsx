15d34a5542be45d0e0f4e2218e96c6c18d8de118
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-05 13:06:49 +0200

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import s from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={s.footer}>
+            <div>
+                Info
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
