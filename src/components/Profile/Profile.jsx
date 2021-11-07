1dd8e33b420fbd7ff0b996c5bb8571a4183ba39e
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-07 02:03:26 +0200

    Move main content styles to App.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,18 @@
 import PostsSection from './PostsSection/PostsSection';
 import s from './Profile.module.css';
 
-const Profile = () => {
+const Profile = (props) => {
     return (
-        <div className={s.profile}>
-            <div>
+        <div>
+            <div className={s.top}>
                 <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
             </div>
-            <div>
+            <div className={s.mid}>
                 ava + description
             </div>
-           <PostsSection />
+            <div className={s.bot}>
+                <PostsSection />
+            </div>
         </div>
     );
 }
