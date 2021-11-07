935c62c12b77c6203d4add59e69bd75cac2c508e
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-07 02:01:54 +0200

    Add content to Post

--- src/components/Profile/PostsSection/Post/Post.jsx
+++ src/components/Profile/PostsSection/Post/Post.jsx
@@ -4,7 +4,11 @@ const Post = (props) => {
     console.log(props);
     return (
         <div className={s.post}>
-            {props.value.name}
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpyC62gxaLN1W1BmYQzMqmBnM3qJ28exYDgQ&usqp=CAU' />
+            {props.mes}
+            <div className={s.like}>
+                &#9731;&nbsp;{props.likes}
+            </div>
         </div>
     );
 }
