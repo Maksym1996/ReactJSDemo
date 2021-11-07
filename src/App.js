88455d14f6312a4c818fbf59fd63d0686f7e6060
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-07 02:03:51 +0200

    Add Dialogs component
    Signed-off-by: Maksym Kordonets <maksym_kordonets@epam.com>

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/SIdebar';
 import Footer from './components/Footer/Footer';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>   
+    <div className='app-wrapper'>
       <Header />
       <Sidebar />
-      <Profile />
-      <Footer />   
-    </div>
+      <Footer />
+      <div className='app-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
+    </div >
   );
 }
 
