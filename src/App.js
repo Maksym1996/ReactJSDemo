581ea2c1f8624a18f7c6e01ba33d00da00171333
Maksym Kordonets <maksym_kordonets@epam.com>
2021-11-17 10:23:55 +0200

    Rename containers

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import SidebarContainer from './components/Sidebar/SidebarReduxContainer';
-import DialogsContainer from './components/Dialogs/DialogsReduxContainer';
-import ProfileContainer from './components/Profile/ProfileReduxContainer';
+import SidebarReduxContainer from './components/Sidebar/SidebarReduxContainer';
+import DialogsReduxContainer from './components/Dialogs/DialogsReduxContainer';
+import ProfileReduxContainer from './components/Profile/ProfileReduxContainer';
 
 const App = (props) => {
   return (
     <div className='app-wrapper'>
       <Header />
-      <SidebarContainer />
+      <SidebarReduxContainer />
       <Footer />
       <div className='app-wrapper-content'>
         <Routes>
           <Route path='/dialogs' >
-            <Route path='' element={<DialogsContainer />} />
+            <Route path='' element={<DialogsReduxContainer />} />
             <Route path=':message' element={<Dialogs />} />
           </Route>
-          <Route path='/profile' element={<ProfileContainer />} />
+          <Route path='/profile' element={<ProfileReduxContainer />} />
           <Route path='/news' element={<News />} />
           <Route path='/music' element={<Music />} />
           <Route path='/settings' element={<Settings />} />
