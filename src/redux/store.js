9cd2fcaab833f78421242e6a6c178079d5bbd8a5
Maksym <maksym_kordonets@epam.com>
2021-12-11 03:03:36 +0200

    Add form reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,15 +5,19 @@ import profileReducer from './reducers/profileReducer';
 import sidebarReducer from './reducers/sidebarReducer';
 import userReducer from './reducers/userReducer';
 import thunkMiddleware from 'redux-thunk';
+import {reducer as reducerForm} from 'redux-form';
 
 let rootReducer = combineReducers({
     profileComponent: profileReducer,
     dialogComponent: dialogReducer,
     sidebarComponent: sidebarReducer,
     usersComponent: userReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: reducerForm
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+window.store = store;
 
 export default store;
