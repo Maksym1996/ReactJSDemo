c3dcd349b32453ba2835a2799104cd0353c1dd7a
Maksym <maksym_kordonets@epam.com>
2021-12-11 03:05:20 +0200

    Add securityAPI.js

---
+++ src/api/securityAPI.js
@@ -0,0 +1,20 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/security/'
+});
+
+const getCaptchaUrl = () => {
+    return instance.get(`get-captcha-url`)
+        .then(response => {
+            return response.data;
+        })
+        .then(data => {
+            return data.url
+        })
+}
+
+export default {
+    getCaptchaUrl
+}
