1bc41f5f20010169d131cf931bbd07a2e3f5ed9f
Maksym <maksym_kordonets@epam.com>
2021-12-11 03:04:58 +0200

    Implement login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { reduxForm, Field } from 'redux-form';
+import authAPI from "../../api/authAPI";
+import securityAPI from "../../api/securityAPI";
 
-const Login = (props) => {
+const LoginForm = (props) => {
+    const [captcha, setCaptcha] = useState();
+
+    useEffect(() => {
+        props.captcha.then(c => {
+            setCaptcha(c);
+        })
+    },[])
     return (
-        <h1>
-            LOGIN
-        </h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder='login' name='email' component='input'/>
+            </div>
+            <div>
+                <Field placeholder='password' name='password' component='input' />
+            </div>
+            <div>
+                <Field type='checkbox' name='rememberMe' component='input'/> remember me
+            </div>
+            <div>
+                <img src={captcha} alt='captcha' />
+            </div>
+            <div>
+                <Field name='captcha' component='input' />
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
 
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        authAPI.loginToSystem(formData).then(data => {
+            console.log(data)
+        });
+    }
+
+   const captcha = securityAPI.getCaptchaUrl();
+    
+    return (
+        <div>
+            <hi>LOGIN</hi>
+            <LoginReduxForm onSubmit={onSubmit} captcha={captcha}/>
+        </div>
+    )
+}
 export default Login;
