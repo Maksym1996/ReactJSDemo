77fe41fd3ec4fcf0be012d9b83c28c6e53c0d4ca
Maksym <maksym_kordonets@epam.com>
2021-12-11 03:04:46 +0200

    Add method loginToSystem to authAPI

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -2,16 +2,24 @@ import * as axios from 'axios';
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/auth/me'
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/auth/'
 });
 
 const getCurrentUser = () => {
-    return instance.get()
+    return instance.get(`me`)
+        .then(response => {
+            return response.data
+        })
+}
+
+const loginToSystem = (loginData) => {
+    return instance.post(`login`, loginData)
         .then(response => {
             return response.data
         })
 }
 
 export default {
-    getCurrentUser
+    getCurrentUser,
+    loginToSystem
 }
